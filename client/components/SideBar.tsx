04f549450e86d0f1fbf26bdeedd0677bd3423f35
Nikunj <nikunjskabra@gmail.com>
2025-07-22 00:03:47 +0530

    fix

--- client/components/SideBar.tsx
+++ client/components/SideBar.tsx
@@ -9,11 +9,14 @@ export function SideBar({habits, OnClick}:{habits:HabitType[],OnClick: ()=> void
     return(
         <div>
             <button className="p-4 pr-35 px-6 mx-2 rounded-xl font-bold cursor-pointer hover:bg-zinc-900" onClick={OnClick}>Add Habit</button>
-            {habits.map((habit, index) => (
-                <div key={index} className="">
-                    <SidebarItem href={`/habit/${habit.id}`} title={habit.name}/>
-                </div>
-            ))}
+            {habits && habits.length > 0 ? (habits.map((habit, index) => (
+                    <div key={index} className="">
+                        <SidebarItem href={`/habit/${habit.id}`} title={habit.name}/>
+                    </div>
+                ))
+            ):(
+                <p>No Habits</p>
+            )}
         </div>
     )
 }
